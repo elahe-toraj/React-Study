e85dfd6a19692f18121adc9c7d88e8e895106d35
elahe toraj | الهه ترج <torajelahe@gmail.com>
2025-02-14 23:09:15 +0330

    add jsx

--- React/components/camelCase/react.jsx
+++ React/components/camelCase/react.jsx
@@ -3,3 +3,5 @@ const inputElement = <input autoFocus />;
 const buttonElement = <button onClick ={() => console.log('clicked')}>click me!</button>
 
 const imageElement = <img src="./image.jpg" />;
+
+const paragraph = <p className="text-red">text</p>;
