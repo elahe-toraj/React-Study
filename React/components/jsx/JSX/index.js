8396dc1ec8f7675e6ab93a7f3ab48e86907a82ec
elahe toraj | الهه ترج <torajelahe@gmail.com>
2025-02-14 22:36:38 +0330

    add jsx

---
+++ React/components/jsx/JSX/index.js
@@ -0,0 +1,7 @@
+const title = <h1>hello react!</h1>;
+
+const container = (
+    <div>
+    <h1>title</h1>
+    </div>
+);
---
+++ React/components/jsx/JavaScript/react.jsx
@@ -0,0 +1,5 @@
+const name = "ali";
+const title = <h1>hello {name}</h1>;
+
+const array = [19, 89, 99];
+const paragraph = <p>the number is {array[2]}</p>;
