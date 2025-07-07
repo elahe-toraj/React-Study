c8c4467d300910201a5266930c72db9b5aef2aa3
elahe-toraj <torajelahe@gmail.com>
2025-07-08 02:06:46 +0800

    edit-validation2

--- React/components/Validation2/src/hooks/useValidation.ts
+++ React/components/Validation2/src/hooks/useValidation.ts
@@ -1,16 +1,12 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function useValidation() {
-  const [value, setValue] = useState('');
+function useValidation(initialValue: string) {
+  const [value, setValue] = useState(initialValue);
   const [isValid, setIsValid] = useState(false);
 
   useEffect(() => {
-    const year = parseInt(value);
-    if (year >= 1900 && year <= 2024) {
-      setIsValid(true);
-    } else {
-      setIsValid(false);
-    }
+    const year = Number(value);
+    setIsValid(year >= 1900 && year <= 2024);
   }, [value]);
 
   return { value, setValue, isValid };
